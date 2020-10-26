ce18d8dbb2fab17cf08a0a12ebf60a38ee2e822f
Clifton Kyle Marasigan <ignisalt3@gmail.com>
2020-10-26 15:20:53 +0800

    Leaf Health Image
    added a feature for leaf health manager

--- Assets/LeafBehavior.cs
+++ Assets/LeafBehavior.cs
@@ -1,21 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeafBehavior : MonoBehaviour
 {
     public Resource Health;
-    public HealthHandler HealthHandlerObj;
-
+    public Image LeafImage;
+    public Sprite[] LeafSprites;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Health.EVT_OnResourceAdded.AddListener(UpdateLeafSprite);
+        Health.EVT_OnResourceSubtracted.AddListener(UpdateLeafSprite);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateLeafSprite()
     {
-        
+        if (Health.Value >= 50 && Health.Value <= 100)
+        {
+            LeafImage.sprite = LeafSprites[0];
+        }
+        if (Health.Value >=20 && Health.Value <=50)
+        {
+            LeafImage.sprite = LeafSprites[1];
+        }
+        if (Health.Value >= 0 && Health.Value <= 20)
+        {
+            LeafImage.sprite = LeafSprites[2];
+        }
     }
+
+
 }
