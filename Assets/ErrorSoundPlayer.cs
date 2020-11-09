e26d7483da544e15860c964241cf393258374ed7
Clifton Kyle Marasigan <ignisalt3@gmail.com>
2020-11-09 19:13:37 +0800

    Added more feedback
    -particles added
    -added error feedback
    -added red miss text

---
+++ Assets/ErrorSoundPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErrorSoundPlayer : MonoBehaviour
+{
+    public BeatSpawner BeatSpawnerObj;
+    public AudioSource ErrorSoundSource;
+    public Sequencer SequencerObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BeatSpawnerObj.EVT_OnBeatPooled.AddListener(PlayErrorSoundOnMiss);
+    }
+
+    void PlayErrorSoundOnMiss(Beat BeatObjReference)
+    {
+        if (BeatObjReference.Status == BeatState.Early || BeatObjReference.Status == BeatState.Late)
+        {
+            StartCoroutine(WaitToPlay());
+        }
+        else return;
+    }
+
+    IEnumerator WaitToPlay()
+    {
+        yield return new WaitForSeconds(SequencerObj.OffSetBeatTime / 4);
+        ErrorSoundSource.Play();
+    }
+}
---
+++ Assets/ParticleSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class ParticleSpawner : ObjectPooler
+{
+    public float ParticleDuration;
+    public BeatSpawner BeatSpawnerObj;
+    public BeatState BeatStatusCondition;
+
+    private void Start()
+    {
+        BeatSpawnerObj.EVT_OnBeatPooled.AddListener(SpawnObjectBasedOnBeat);
+    }
+    protected override void InitializeSpawnObject(GameObject obj)
+    {
+        
+        StartCoroutine(DespawnInDuration(obj));
+    }
+
+    private void SpawnObjectBasedOnBeat(Beat BeatObjReference)
+    {
+        SpawnPosition = BeatObjReference.transform.position;
+        if (BeatObjReference.Status == this.BeatStatusCondition)
+        {
+            SpawnObjects();
+        }
+    }
+    private void OnDeactivate(GameObject obj)
+    {
+        obj.gameObject.SetActive(false);
+        pooledObjects.Add(obj.gameObject);
+        currentSpawnedObjects.Remove(obj.gameObject);
+    }
+    IEnumerator DespawnInDuration(GameObject objToDespawn)
+    {
+        yield return new WaitForSeconds(ParticleDuration);
+        OnDeactivate(objToDespawn);
+    }
+
+}
